2c4ba2290280a571bdef89f051df06c3766b4aac
calvi <calvi@LAPTOP-0M6T3Q6C>
2022-09-04 14:35:25 +1000

    inventory UML diagrams

--- YPapp/src/main/java/com/mycompany/ypapp/YPapp.java
+++ YPapp/src/main/java/com/mycompany/ypapp/YPapp.java
@@ -1,14 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
- */
-
 package com.mycompany.ypapp;
 
-/**
- *
- * @author calvi
- */
 public class YPapp {
 
     public static void main(String[] args) {
